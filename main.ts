8c35c64eb7feac754f9b4e4895ea1c375e9193cd
Carlos Arellano <luis.harellano@alumnos.udg.mx>
2020-05-19 23:41:25 -0500

    Initial commit

---
+++ main.ts
@@ -0,0 +1,11 @@
+import { RequestHandler } from './network/requestHandler.ts';
+
+export class Bot {
+  private token: string;
+
+  constructor(token: string) {
+    this.token = token; 
+  }
+
+  
+}
